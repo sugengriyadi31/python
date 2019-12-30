5a2fb4d5cadb82012ae3ffd4a8f82f877ad906d0
Sugeng Riyadi <sugengriyadi@adsl-172-10-101-223.dsl.hstntx.sbcglobal.net>
2019-12-30 17:51:01 +0700

    first commit

---
+++ if else.py
@@ -0,0 +1,20 @@
+nilai = 101
+
+if nilai >=80 and nilai <=100:
+    print("Nilai kamu A")
+elif nilai >=75 and nilai <80:
+    print("Nilai Kamu B+")
+elif nilai >=70 and nilai <75:
+    print("Nilai Kamu B")
+elif nilai >=65 and nilai <70:
+    print("Nilai Kamu C+")
+elif nilai >=60 and nilai <65:
+    print("Nilai Kamu C")
+elif nilai >=55 and nilai <60:
+    print("Nilai Kamu D+")
+elif nilai >=50 and nilai <55:
+    print("Nilai Kamu D")
+elif nilai >=0 and nilai <50:
+    print("Nilai Kamu E")    
+else:
+    print("Nilai yang dimasukkan salah")
