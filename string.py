89a9f11cca235f9b156f3623d96d8c729a7f8b38
Sugeng Riyadi <sugengriyadi@Sugengs-MacBook-Air.local>
2020-01-03 14:36:40 +0700

    Menambahkan File Tentang Membaca List dalam Array

---
+++ string.py
@@ -0,0 +1,6 @@
+gorengan = ["bakwan", "tahu", "cireng","pisang", "risoles"]
+beli = "sepatu"
+if beli in gorengan:
+    print ('Mamang bilang, "ya saya jual',beli,'"')
+if not beli in gorengan:
+    print ('Mamang bilang, "maaf saya tidak jual',beli,'"')
