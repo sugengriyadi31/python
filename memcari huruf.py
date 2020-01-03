2e5b53c7aa5dcae1e97baff6d0fd20c48b0ae5b4
Sugeng Riyadi <sugengriyadi@Sugengs-MacBook-Air.local>
2020-01-03 14:42:30 +0700

    Menambahkan file mencari huruf

---
+++ memcari huruf.py
@@ -0,0 +1,6 @@
+kalimat = "belajar python sangat menyenangkan"
+karakter = "z"
+if karakter in kalimat:
+    print("ada huruf",karakter,"di kalimat",kalimat)
+if not karakter in kalimat:
+    print("tidak ada huruf",karakter,"di kalimat",kalimat)
