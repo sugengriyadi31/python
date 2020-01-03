de473852940b4df7b424c8bb641ee13ab5f0156f
Sugeng Riyadi <sugengriyadi@Sugengs-MacBook-Air.local>
2020-01-03 18:00:29 +0700

    Menambahkan File For Loop

---
+++ for loop.py
@@ -0,0 +1,8 @@
+merk = ['xiaomi', 'nokia', 'samsung', 'asus', 'oppo', 'vivo', 'realme', 'huawei', 'sony', 'google pixel']
+for o in merk:
+    print(o)
+    print(len(o))
+print("*"*100)
+type = "redmi"
+for i in type:
+    print(i)
