7c03612740b103454c6d86880e93d65e0c5ceade
Sugeng Riyadi <sugengriyadi@Sugengs-MacBook-Air.local>
2020-01-08 16:04:57 +0700

    menambahkan continue dan pass

---
+++ continue dan pass.py
@@ -0,0 +1,9 @@
+for i in range(1,10):
+    if i == 5:
+        print('nilai saat ini adalah 5')
+        continue
+    print('nilai' , i)
+else:
+    print('akhir dari loop')
+
+print('print diluar loop')    
---
+++ for else, range, break.py
@@ -0,0 +1,6 @@
+mencari_angka = 25  
+for i in range (10,40):
+    print (i)
+    if i is 25:
+        print('angka ditemukan',i)
+        break
